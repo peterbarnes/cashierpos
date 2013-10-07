1192b7c2e6546a6dc45620e7ad76a9334e11479f
Aaron Wright <acwrightdesign@gmail.com>
2013-10-07 15:39:52 -0500

    Update...

--- bootstrap.rb
+++ bootstrap.rb
@@ -14,9 +14,9 @@ Dir['lib/**/*.rb'].each {|file| require_relative file }
 Paperclip::Attachment.default_options.merge!(
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['AS3_BUCKET'],
-    :access_key_id => ENV['AS3_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AS3_SECRET_ACCESS_KEY']
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 )
 
