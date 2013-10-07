7264bbc0d7f9582a6f37bf2e52033669f44fd0a5
Aaron Wright <acwrightdesign@gmail.com>
2013-10-07 15:44:47 -0500

    Update...

--- bootstrap.rb
+++ bootstrap.rb
@@ -11,14 +11,16 @@ require 'autoinc'
 
 Dir['lib/**/*.rb'].each {|file| require_relative file }
 
-Paperclip::Attachment.default_options.merge!(
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  },
-  :s3_host_name => 's3-us-west-2'
-)
+if Cashierpos.production?
+  Paperclip::Attachment.default_options.merge!(
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :s3_host_name => 's3-us-west-2.amazonaws.com'
+  )
+end
 
 require_relative 'cashierpos'
