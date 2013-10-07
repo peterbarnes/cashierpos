1ee5b62a7f5bfa696eb2e24b9bc1789885a0b5e0
Aaron Wright <acwrightdesign@gmail.com>
2013-10-07 15:42:35 -0500

    Update...

--- bootstrap.rb
+++ bootstrap.rb
@@ -17,7 +17,8 @@ Paperclip::Attachment.default_options.merge!(
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+  },
+  :s3_host_name => 's3-us-west-2'
 )
 
 require_relative 'cashierpos'
